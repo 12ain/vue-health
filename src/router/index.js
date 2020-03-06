d5dd73cc4b9844fcdbcdcddf5f073fe0d98a5434
xiaoshiguang123 <xyt123@163.com>
2020-03-06 22:36:30 +0800

    :new: 初始化项目

--- src/router/index.js
+++ src/router/index.js
@@ -10,18 +10,16 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  // 路由懒加载
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About.vue')
   }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes
 })
